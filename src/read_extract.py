9c0bf18da511ffa32a359bed8624938e9de6a7f0
devDataEngineer <oleg.dachcenter@gmail.com>
2024-08-15 15:51:35 +0100

    update the read_extract file with adding the utilities part for converting the output +tests to utilities function

--- src/read_extract.py
+++ src/read_extract.py
@@ -1,23 +1,27 @@
 from src.connection import close_db_connection, db_connection
+from utilities.utilities import format_extract_lambda_as_rows
 from pg8000 import DatabaseError
+
 
 def read_table(db_table):
 
-    table_whitelist = ['counterparty', 'currency', 'department', 'design', 'staff', 
-                       'sales_order', 'address', 'payment', 'purchase_order', 'payment_type', 'transaction']
+    table_whitelist = ['counterparty', 'currency', 'department', 'design', 
+                       'staff', 'sales_order', 'address', 'payment', 
+                       'purchase_order', 'payment_type', 'transaction']
     
     if db_table not in table_whitelist:
         raise ValueError(f"Invalid table name: {db_table}")
     
     try:
         conn = db_connection()
-        staff_query = f"SELECT * FROM {db_table};"
-        staff_table = conn.run(staff_query)
-        return staff_table
+        query = f"SELECT * FROM {db_table};"
+        rows = conn.run(query)
+        column_list = [conn.columns[i]['name'] for i in range(len(conn.columns))]
+        formatted = format_extract_lambda_as_rows(rows,column_list)
+        return formatted
     
     except DatabaseError as e:
         raise e
     
     finally:
        close_db_connection(conn)
-
---
+++ utilities/utilities.py
@@ -0,0 +1,22 @@
+# from src.read_extract import db_table
+
+def format_extract_lambda_as_rows(rows_list, columns_list):
+    """ Formatting output in list of dictionaries with seperate
+        dictionaries representing rows of <column:value> pairs:
+
+       [{'column1': 'value1', 'column2': 'value2'}, 
+        {'column1': 'value3', 'column2': 'value4'}, 
+        {'column1': 'value5', 'column2': 'value6}]
+        """
+    formatted = [dict(zip(columns_list,row)) for row in rows_list]
+    return formatted
+
+# def format_extract_lambda_as_columns(rows_list, columns_list):
+#     """ Formatting output in list of dictionaries with a single dict
+#         having <column_name:[list_of_values]> pairs:
+
+#        {column1:['value1','value2'], column2:['value1','value2'], 
+#        column3:['value1','value2']}
+#         """
+    
+#     # TBD....
