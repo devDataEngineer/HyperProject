384a9de2ec7e8891a811c6c56a7784d428493b5f
Charlotte Handford <handford.charlotte@yahoo.co.uk>
2024-08-20 16:44:31 +0100

    delete redundant test file and fix most failing tests

--- test/test_extract.py
+++ test/test_extract.py
@@ -37,28 +37,26 @@ def s3_client(aws_creds):
 #         s3_resource = boto3.resource("s3")
 
 
-@pytest.mark.skip()
 @patch('src.extractlambda.extract.db_connection')
 def test_function_returns_correctly_formatted_table(mock_conn):
     mock_conn.return_value.run.return_value = [["a","b","c", "d"],[1, 2, 3, 4]]
     mock_conn.return_value.columns = [{"name": "a"}, {"name": "b"}, {"name": "c"}, {"name": "d"} ]
     result = read_table("counterparty", datetime(2024, 8, 15, 13, 21, 10), datetime(1990, 8, 15, 13, 21, 10))
-    assert isinstance(result[0], list)
-    assert result[0] == [{'a': 'a', 'b': 'b', 'c': 'c', 'd': 'd'}, {'a': 1, 'b': 2, 'c': 3, 'd': 4}]
+    assert isinstance(result, list)
+    assert result == [{'a': 'a', 'b': 'b', 'c': 'c', 'd': 'd'}, {'a': 1, 'b': 2, 'c': 3, 'd': 4}]
 
 def test_function_raises_ValueError_for_non_whitelist_table():
     with pytest.raises(ValueError):
         read_table("nonexist", datetime(2024, 8, 15, 13, 21, 10), datetime(1990, 8, 15, 13, 21, 10))
 
-@pytest.mark.xfail(reason="This isn't raising DB error - related to finally?")
+# @pytest.mark.xfail(reason="This isn't raising DB error - related to finally?")
 @patch('src.extractlambda.extract.db_connection')
 def test_function_correctly_raises_DatabaseError(mock_conn):
-        mock_conn.side_effect = Exception(DatabaseError)
+        mock_conn.return_value.run.side_effect = DatabaseError
         with pytest.raises(DatabaseError):
             read_table("department", datetime(2024, 8, 15, 13, 21, 10), datetime(1990, 8, 15, 13, 21, 10))
 
-@pytest.mark.skip("Skip now ")
-def test_load_fun_create_json_file(s3_client):
+def test_load_table_create_json_file(s3_client):
     fake_data = [{'id': 1, "name": "G"}]
     date_to_compare = datetime(1990, 8, 15, 13, 21, 10)
     date_to_compare2 = datetime(1990, 8, 15, 13, 21, 12)
@@ -67,21 +65,27 @@ def test_load_fun_create_json_file(s3_client):
     s3_client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={'LocationConstraint': 'eu-west-2'})
     load_table(db_name, fake_data, date_to_compare)
     load_table(db_name, fake_data, date_to_compare2)
-    # Verify the file was uploaded correctly
     obj = s3_client.list_objects(Bucket=bucket_name)
     assert obj['Contents'][0]['Key'] == "staff/1990/08/15/13-21-10.json"
+    assert obj['Contents'][1]['Key'] == "staff/1990/08/15/13-21-12.json"
+
+def test_load_all_tables_returns_dict(s3_client):
+    date_to_compare = datetime(1990, 8, 15, 13, 21, 10)
+    date_to_compare2 = datetime(1990, 8, 15, 13, 21, 12)
+    with patch ("src.extractlambda.extract.load_table") as pl:
+        with patch ("src.extractlambda.extract.read_table") as pr:
+            pr.return_value = [
+                {'column1': 'value1', 'column2': 'value2'}, 
+                {'column1': 'value3', 'column2': 'value4'}, 
+                {'column1': 'value5', 'column2': 'value6'}
+                ]
+            pl.return_value = None
+            assert isinstance(load_all_tables(date_to_compare, date_to_compare2), dict)
 
 
-def test_lambra_handler_returns_list(s3_client):
+def test_lambda_handler_returns_dict(s3_client):
     with patch ("src.extractlambda.extract.load_all_tables") as p:
-        p.return_value = [
-            'counterparty', 'currency', 'department',
-            'design', 'staff', 'sales_order',
-            'address', 'payment', 'purchase_order',
-            'payment_type', 'transaction'
-            ]
-        assert isinstance(lambda_handler("", ""), list)
-
-@pytest.mark.skip()
-def test_load_all_tables_returns_list(s3_client):
-    pass
+        p.return_value = {
+            'counterparty': "link", 'currency': "link", 'department': "link"
+                            }
+        assert isinstance(lambda_handler("", ""), dict)
