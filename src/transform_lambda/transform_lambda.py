41b96a068c357c8e2759f798eb6aa30604a35145
Zabihullah Habibi <zabihullahhabibi@Zabihullahs-MBP.northcoders.com>
2024-08-21 14:54:22 +0100

    create data frame dim table of currency and date is done

--- src/transform_lambda/transform_lambda.py
+++ src/transform_lambda/transform_lambda.py
@@ -4,25 +4,26 @@ import boto3.exceptions
 import pandas as pd
 import io
 
-def get_data(file_path):
-
+def get_data(file_path: str):
    s3_client = boto3.client('s3') 
-   objject_lsit = []
    SOURCE_BUCKET = 'Ingestion_bucket'
-   DESTINATION_BUCKET = 'Transform_bucket'
+   file_key = file_path
    try :
-    
-    file = s3_client.get_object(Bucket=SOURCE_BUCKET, Key=file_path)
-    return file
+      if file_path.startswith("S3://"):
+         file_path = file_path[5:]
+         SOURCE_BUCKET, file_key = file_path.split('/',1)
+      file = s3_client.get_object(Bucket=SOURCE_BUCKET, Key=file_key)
+      return file
    except Exception as e:
-        print("Ingestion bucket is empty: " + str(e))
+        print("File path is wrong: " + str(e))
+        raise e
 
-# ---------[test purpose]convert json to df-------------------#
-def convert_json_to_df(jsonfile):
-   data = jsonfile['Body'].read().decode('utf-8')
-   string_io = io.StringIO(data)
-   df = pd.read_json(string_io, orient='records')
-   return df
+# # ---------[test purpose]convert json to df-------------------#
+# def convert_json_to_df(jsonfile):
+#    data = jsonfile['Body'].read().decode('utf-8')
+#    string_io = io.StringIO(data)
+#    df = pd.read_json(string_io, orient='records')
+#    return df
 
 
 def create_fact_sales_order_df(df_fact_slae):
@@ -30,5 +31,32 @@ def create_fact_sales_order_df(df_fact_slae):
 
 #----------formate dim_currency data frame----------------------#
 def create_df_dim_currency(df_currency):
-   dim_currency = df_currency.drop(columns=['created_at','last_update'])
+   dim_currency = df_currency
+   currency_names = {
+            'GBP': 'British Pound',
+            'USD': 'US Dollar',
+            'EUR': 'Euro',
+            'CHF': 'Swiss Franc'
+        }
+   dim_currency['currency_name'] = dim_currency['currency_code'].apply(lambda x: currency_names[x])
+   dim_currency = dim_currency.drop(columns = ['created_at','last_update'])
+   dim_currency.set_index= dim_currency['currency_id']
+   dim_currency.name = "dim_currency"
    return dim_currency
+
+#----------formate dim_date_df data frame----------------------#
+def create_df_dim_date(df_date):
+   dim_date = df_date
+   dim_date['year'] = pd.DatetimeIndex(dim_date['created_date']).year
+   dim_date['month'] = pd.DatetimeIndex(dim_date['created_date']).month
+   dim_date['day'] = pd.DatetimeIndex(dim_date['created_date']).day
+   dim_date['day_of_week'] = pd.DatetimeIndex(dim_date['created_date']).day_of_week
+   dim_date['day_name'] = dim_date['created_date'].dt.day_name()
+   dim_date['month_name'] = dim_date['created_date'].dt.month_name()
+   dim_date['quarter'] = pd.DatetimeIndex(dim_date['created_date']).quarter
+   dim_date = dim_date.rename(columns = {'created_date': "date_id"})
+   dim_date.set_index = dim_date['date_id']
+   dim_date.name = 'dim_date'
+   return dim_date
+
+
--- test/test_transform_lambda.py
+++ test/test_transform_lambda.py
@@ -1,13 +1,13 @@
-from src.transform_lambda.transform_lambda import get_data, convert_json_to_df, create_df_dim_currency
+from src.transform_lambda.transform_lambda import get_data, create_df_dim_currency, create_df_dim_date
 from moto import mock_aws
 import pytest
 import boto3
 import os
 from botocore.exceptions import ClientError
 from dfmock import DFMock
-
-
-
+from datetime import datetime
+import pandas as pd
+import datatest as dt
 
 @pytest.fixture
 def aws_creds():
@@ -40,17 +40,10 @@ def resource_client(aws_cred):
 
 def test_get_data_func_get_file_from_ingestion(s3_client):
     s3_client.put_object(Bucket=BUCKET1, Key=f'{KEY}.json', Body=CONTENT)
-    result = get_data('TEST_KEY.json') 
+    result = get_data(f'S3://Ingestion_bucket/{KEY}.json') 
     result_body = result['Body'].read().decode('utf-8')
     assert result_body == CONTENT
     assert result['ResponseMetadata']['HTTPStatusCode'] == 200
-
-
-# def test_get_data_from_ingestion_bucket_lambda_when_bucket_is_empy(s3_client):
-    
-#     with pytest.raises(ClientError) as excinfo:
-#         get_data("file_path")
-#     assert "Ingestion bucket is empty" in str(excinfo.value)
 
 def test_get_data_empty_file(s3_client):
     file_path = 'empty_file.json'
@@ -61,29 +54,53 @@ def test_get_data_empty_file(s3_client):
     assert result['ContentLength'] == 0
     assert result['ResponseMetadata']['HTTPStatusCode'] == 200
 
-#--------------test for convert function------------------#
+# @pytest.mark.skip("need Mick to help us")
+def test_get_data_from_ingestion_bucket_lambda_when_bucket_is_empy(s3_client):
+    
+    with pytest.raises(ClientError) as excinfo:
+        get_data("file_path")
 
-def test_convert_json_to_df(s3_client):
-    s3_client.put_object(Bucket=BUCKET1, Key=f'{KEY}.json', Body=CONTENT)
-    json_file = get_data(f'{KEY}.json')
-    convert_json_to_df(json_file)
+
+
+# #--------------test for convert function------------------#
+
+# def test_convert_json_to_df(s3_client):
+#     s3_client.put_object(Bucket=BUCKET1, Key=f'{KEY}.json', Body=CONTENT)
+#     json_file = get_data(f'{KEY}.json')
+#     convert_json_to_df(json_file)
     
 #-----test for formating dim_currency data frame-------#
 def test_df_dim_currency(s3_client):
-    columns = { "currency_id":"integer",
-               "currency_code": "string",
-               "currency_name": "string",
-               "created_at": "datetime",
-            "last_update":"datetime"
-          }
-    dfmock = DFMock(count=100, columns=columns)
-    dfmock.generate_dataframe()
-    my_mocked_dataframe = dfmock.dataframe
-    result = create_df_dim_currency(my_mocked_dataframe)
-    assert "created_at" not in result
-    assert "currency_id" in result
+    # columns = { "currency_id":"integer",
+    #            "currency_code": "string",
+    #            "created_at": "datetime",
+    #         "last_update":"datetime"
+    #       }
+    # dfmock = DFMock(count=5, columns=columns)
+    # dfmock.generate_dataframe()
+    # my_mocked_dataframe = dfmock.dataframe
+    data = {'currency_id':[1,2,3,4],
+        'currency_code':['GBP','USD','EUR','CHF'],
+        'created_at': [datetime.now(),datetime.now(),datetime.now(),datetime.now()],
+        'last_update':[datetime.now(),datetime.now(),datetime.now(),datetime.now()]
+                       }
+    df = pd.DataFrame(data)
+    result = create_df_dim_currency(df)
+    assert "last_update" not in result.columns
+    assert "currency_id" in result.columns
 
 
+#-----test for formating dim_date data frame-------#
+def test_df_dim_date(s3_client):
+    data = { 
+        'created_date':[datetime.now()]
+    }
+    df = pd.DataFrame(data)
+    result = create_df_dim_date(df)
+    dt.validate(
+        result.columns, {'date_id', 'year','month','day','day_of_week','day_name','month_name','quarter'}
+    )
+    assert 'created_date' not in result.columns
 
 
 
