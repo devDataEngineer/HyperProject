6b44c249038d72d1753eb91cb63c1c41c81b1ff4
chandford <160608163+chandford@users.noreply.github.com>
2024-08-21 11:18:46 +0100

    Update ssm.tf

--- terraform/ssm.tf
+++ terraform/ssm.tf
@@ -1,4 +1,4 @@
-resource "aws_ssm_parameter" "ssm_parmeter" {
+resource "aws_ssm_parameter" "ssm_parameter" {
   name  = var.ssm_parameter_name
   type  = "String"
   value = "1990-01-01 10:10:10.111111"
