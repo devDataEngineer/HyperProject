ad2bf3085ec6d5f0d2562435a88d72cb651c3416
Zabihullah Habibi <zabihullahhabibi@Zabihullahs-MBP.northcoders.com>
2024-08-22 14:55:39 +0100

    get_data function is moved to seprate python file

--- src/transform_lambda/transform_lambda.py
+++ src/transform_lambda/transform_lambda.py
@@ -9,21 +9,6 @@ logger = logging.getLogger()
 logger.setLevel("INFO")
 
 
-def get_data(file_path: str):
-   s3_client = boto3.client('s3') 
-   SOURCE_BUCKET = 'team-hyper-accelerated-dragon-bucket-ingestion'
-   file_key = file_path
-   try :
-      if file_path.startswith("s3://"):
-         file_path = file_path[5:]
-         SOURCE_BUCKET, file_key = file_path.split('/',1)
-      logger.info(f"Reading file {file_path} from the Ingestion s3 bucket")
-      file = s3_client.get_object(Bucket=SOURCE_BUCKET, Key=file_key)
-      return file
-   except Exception as e:
-      logger.error(f"Error occured during reading the file {file_path}. More info:" + str(e))
-      raise e
-
 
 #----------formate dim_currency data frame----------------------#
 def create_df_dim_currency(df_currency):
--- test/test_transform_lambda.py
+++ test/test_transform_lambda.py
@@ -9,57 +9,6 @@ from datetime import datetime
 import pandas as pd
 import datatest as dt
 
-@pytest.fixture
-def aws_creds():
-    os.environ["AWS_ACCESS_KEY_ID"] = "test"
-    os.environ["AWS_SECRET_ACCESS_KEY"] = "test"
-    os.environ["AWS_SECURITY_TOKEN"] = "test"
-    os.environ["AWS_SESSION_TOKEN"] = "test"
-    os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
-
-BUCKET1 ="Ingestion_bucket"
-BUCKET2 = 'Transform_bucket'
-KEY = "TEST_KEY"
-CONTENT = '''[{"id":1,"first_name":"Reinaldo","last_name":"Pinchbeck","email":"rpinchbeck0@alexa.com","gender":"Male","ip_address":"237.188.238.35"},
-{"id":2,"first_name":"Joshuah","last_name":"Reyes","email":"jreyes1@taobao.com","gender":"Male","ip_address":"242.77.75.252"}]'''
- 
-
-@pytest.fixture
-def s3_client(aws_creds):
-    with mock_aws():
-        s3_client = boto3.client("s3")
-        s3_client.create_bucket(Bucket=BUCKET1, CreateBucketConfiguration={'LocationConstraint': 'eu-west-2'})
-        yield s3_client
-
-@pytest.fixture
-def resource_client(aws_cred):
-    with mock_aws():
-        s3_resource = boto3.resource("s3")
-
-
-
-def test_get_data_func_get_file_from_ingestion(s3_client):
-    s3_client.put_object(Bucket=BUCKET1, Key=f'{KEY}.json', Body=CONTENT)
-    result = get_data(f'S3://Ingestion_bucket/{KEY}.json') 
-    result_body = result['Body'].read().decode('utf-8')
-    assert result_body == CONTENT
-    assert result['ResponseMetadata']['HTTPStatusCode'] == 200
-
-def test_get_data_empty_file(s3_client):
-    file_path = 'empty_file.json'
-    file_content = b''  # Empty content
-    s3_client.put_object(Bucket=BUCKET1, Key=file_path, Body=file_content)
-    result = get_data(file_path)
-    assert result['Body'].read() == file_content
-    assert result['ContentLength'] == 0
-    assert result['ResponseMetadata']['HTTPStatusCode'] == 200
-
-# @pytest.mark.skip("need Mick to help us")
-def test_get_data_from_ingestion_bucket_lambda_when_bucket_is_empy(s3_client):
-    
-    with pytest.raises(ClientError) as excinfo:
-        get_data("file_path")
-
     
 #-----test for formating dim_currency data frame-------#
 def test_df_dim_currency():
