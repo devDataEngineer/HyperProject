82ab3c647b80bb7ae896fb7ca070bf518dc975ce
bokney <toknyk@gmail.com>
2024-08-22 16:38:33 +0100

    renamed misnamed function

--- src/transformlambda/transform.py
+++ src/transformlambda/transform.py
@@ -5,10 +5,10 @@ import logging
 logger = logging.getLogger()
 logger.setLevel("INFO")
 
-# example_argument = {
-#     "counterparty": "counterparty/2024/08/20/12-00-00.json",
-#     "currency": "currency/2024/08/20/12-00-00.json"
-# }
+example_argument = {
+    "counterparty": "counterparty/2024/08/20/12-00-00.json",
+    "currency": "currency/2024/08/20/12-00-00.json"
+}
 
 # -------------------------------------------------------------------------- #
 
@@ -33,9 +33,9 @@ def convert_dataframe_to_parquet(dataframe: pd.DataFrame) -> bytes:
     """
     pass
 
-def upload_to_transform_bucket(parquet_file: bytes, filename: str) -> None:
+def upload_to_processed_bucket(parquet_file: bytes, filename: str) -> None:
     """Takes parquet as bytes and a filename
-    Puts parquet into Transform S3 Bucket named as filename"""
+    Puts parquet into Processed S3 Bucket named as filename"""
     pass
 
 # -------------------------------------------------------------------------- #
@@ -157,7 +157,7 @@ def lambda_handler(event, context) -> None:
     logger.info("Uploading parquet files to Processed S3 Bucket")
     for df in dataframe_list:
         logger.info(f"Uploading {df}...")
-        upload_to_transform_bucket(
+        upload_to_processed_bucket(
             dataframe_parquet_filepaths[df],
             "FILENAME_GOES_HERE"
             )
