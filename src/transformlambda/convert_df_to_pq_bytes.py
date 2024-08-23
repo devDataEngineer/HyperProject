6fc21be1842a1d7e7d81ccd9cdbdc1f0e11f736c
chandford <160608163+chandford@users.noreply.github.com>
2024-08-23 15:00:46 +0100

    Update convert_df_to_pq_bytes.py

--- src/transformlambda/convert_df_to_pq_bytes.py
+++ src/transformlambda/convert_df_to_pq_bytes.py
@@ -9,9 +9,9 @@ logger.setLevel("INFO")
 def convert_dataframe_to_parquet_bytes(input_df):
     try:
         if isinstance(input_df, pd.DataFrame):
-            df_bytes = input_df.to_parquet()
+            pq_bytes = input_df.to_parquet()
             logger.info("Df converted to parquet.")
-            return df_bytes
+            return pq_bytes
         else:
             logger.error("Please input a Panda Dataframe.")
             
