a14f1e50c9c6c507399abaf631cf6e8f2b1c5412
GoncaloCid <gonga1973@hotmail.com>
2024-08-23 12:26:20 +0100

    refactores dim_design function and tests(adds loggers and error handler)

--- test/test_dim_design.py
+++ test/test_dim_design.py
@@ -9,8 +9,6 @@ from datetime import datetime
 import pandas as pd
 import datatest as dt
 
-
-
 def test_create_df_dim_design_creates_correct_dataframe():
     data = {'design_id':[1,2],
         'created_at': [datetime.now(), datetime.now()],
@@ -21,7 +19,6 @@ def test_create_df_dim_design_creates_correct_dataframe():
                        }
     df = pd.DataFrame(data)
     
-
     result = create_df_dim_design(df)
 
     assert 'created_at' not in result.columns
@@ -59,13 +56,28 @@ def test_create_df_dim_design_file_location_column_values_are_of_the_type():
     df_design = pd.DataFrame(data)
 
     result = create_df_dim_design(df_design)
-    
+    print(result.dtypes)
     assert result['design_id'].dtypes == int
     assert pd.api.types.is_string_dtype(result['file_location'].dtype) == True
     assert pd.api.types.is_string_dtype(result['design_name'].dtype) == True
     assert pd.api.types.is_string_dtype(result['file_name'].dtype) == True
     
     
+def test_create_df_dim_design_raises_error():
+    data = {'design_id':[1,2],
+        'last_updated':[datetime.now(), datetime.now()],
+        'design_name' : str,
+        'file_location':  str,
+        'file_name' : str
+                       }
+    df_design = pd.DataFrame(data)
+
+    with pytest.raises(AttributeError):
+        create_df_dim_design(df_design)
+
+
+
+
     
     
 
