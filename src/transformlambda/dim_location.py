5c16cb69a9f5ec86cb48e31b802dabbe52ffc26b
chandford <160608163+chandford@users.noreply.github.com>
2024-08-23 10:54:43 +0100

    Update dim_location.py

--- src/transformlambda/dim_location.py
+++ src/transformlambda/dim_location.py
@@ -11,6 +11,6 @@ def create_dim_location(df_fact_sales_order, df_dim_address):
     dim_location = dim_location.drop(columns=['created_at', 'last_updated','address_id'])
     dim_location.name = "dim_location"
 
-    print(dim_location)
+    # print(dim_location)
     return dim_location
     
