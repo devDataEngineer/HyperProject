91b46e59b262910984e87b7a82aa596a6774ac31
bokney <toknyk@gmail.com>
2024-08-26 14:53:14 +0100

    more tiny bugs 🐛 squashed

--- src/transform_lambda/create_df_dim_currency.py
+++ src/transform_lambda/create_df_dim_currency.py
@@ -11,7 +11,7 @@ logger.setLevel("INFO")
 def create_df_dim_currency(df_currency):
     try:
         logger.info("Started processing dim_currency DataFrame")
-        dim_currency = df_currency
+        dim_currency = df_currency.copy()
         currency_names = {
                     'GBP': 'British Pound',
                     'USD': 'US Dollar',
@@ -19,7 +19,7 @@ def create_df_dim_currency(df_currency):
                     'CHF': 'Swiss Franc'
                 }
         dim_currency['currency_name'] = dim_currency['currency_code'].apply(lambda x: currency_names[x])
-        dim_currency = dim_currency.drop(columns = ['created_at','last_update'])
+        dim_currency = dim_currency.drop(columns = ['created_at','last_updated'])
         dim_currency.set_index= dim_currency['currency_id']
         dim_currency.name = "dim_currency"
         logger.info("Finishing processing dim_currency DataFrame")
