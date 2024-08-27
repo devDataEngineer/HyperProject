9e29d5dff04122fb5295c1e49a86c4304a7ff975
Zabihullah Habibi <zabihullahhabibi@Zabihullahs-MacBook-Pro.local>
2024-08-27 10:24:27 +0100

    bug fixed: column last_updated was written last_update

--- test/test_create_df_dim_currency_transform_lambda.py
+++ test/test_create_df_dim_currency_transform_lambda.py
@@ -21,11 +21,11 @@ def test_df_dim_currency_have_correct_cols():
     assert "last_updated" not in result.columns
     assert "currency_id" in result.columns
 
-def test_df_dim_currency_cols_have_correct_dat_type():
+def test_df_dim_currency_cols_have_correct_data_type():
     data = {'currency_id':[1,2,3,4],
         'currency_code':['GBP','USD','EUR','CHF'],
         'created_at': [datetime.now(),datetime.now(),datetime.now(),datetime.now()],
-        'last_update':[datetime.now(),datetime.now(),datetime.now(),datetime.now()]
+        'last_updated':[datetime.now(),datetime.now(),datetime.now(),datetime.now()]
                        }
     df = pd.DataFrame(data)
     result = create_df_dim_currency(df)
@@ -39,7 +39,7 @@ def test_df_dim_currency_raise_error():
     data = {'currency_id':[1,2,3,4],
         'currency_code':['GBP','USD','EUR','AFG'],
         'created_at': [datetime.now(),datetime.now(),datetime.now(),datetime.now()],
-        'last_update':[datetime.now(),datetime.now(),datetime.now(),datetime.now()]
+        'last_updated':[datetime.now(),datetime.now(),datetime.now(),datetime.now()]
                        }
     df = pd.DataFrame(data)
     with pytest.raises(AttributeError):
