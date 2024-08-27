ba4cd2a2cca14a258c790fbc7530ab369c94c877
Oleh Fylypiv <55192127+devDataEngineer@users.noreply.github.com>
2024-08-27 17:33:34 +0100

    Update get_pq_from_bucket.py

--- src/loadlambda/get_pq_from_bucket.py
+++ src/loadlambda/get_pq_from_bucket.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import boto3
-import boto3.exceptions
 import logging
-import pyarrow.parquet as pq
-import botocore
+# import pyarrow.parquet as pq
+from botocore.exceptions import ClientError
 import io
 
 
@@ -22,7 +21,7 @@ def get_pq_from_bucket(bucket_name, key):
         df = pd.read_parquet(io.BytesIO(parquet_data))
         return df
     
-    except botocore.exceptions.ClientError as e:
-        print(f"Error accessing S3: {str(e)}")
-        return None
+    except ClientError as e:
+        logger.info(f"Error accessing S3 Processed bucket: {str(e)}")
+        raise
 
