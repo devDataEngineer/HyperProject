dd0a43fa916b596954f6c8da4c6de4db7b07de6c
Oleh Fylypiv <55192127+devDataEngineer@users.noreply.github.com>
2024-08-27 18:09:57 +0100

    Update get_pq_from_bucket.py

--- src/loadlambda/get_pq_from_bucket.py
+++ src/loadlambda/get_pq_from_bucket.py
@@ -14,14 +14,14 @@ def get_pq_from_bucket(bucket_name, key):
     s3_client = boto3.client('s3')
 
     try:
+        logger.info(f"Reading {key} parquet file from the Processed S3 bucket")
         response = s3_client.get_object(Bucket=bucket_name, Key=key)
         parquet_data = response['Body'].read()
-        
         
         df = pd.read_parquet(io.BytesIO(parquet_data))
         return df
     
     except ClientError as e:
-        logger.info(f"Error accessing S3 Processed bucket: {str(e)}")
+        logger.error(f"Error accessing S3 Processed bucket: {str(e)}")
         raise
 
