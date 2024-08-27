f77aa5596aaedd3d1df50625e78c48e10bc0bfea
GoncaloCid <gonga1973@hotmail.com>
2024-08-27 16:55:59 +0100

    get parquet files from bucket with 2 passing test

---
+++ src/loadlambda/get_pq_from_bucket.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import boto3
+import boto3.exceptions
+import logging
+import pyarrow.parquet as pq
+import botocore
+import io
+
+
+logger = logging.getLogger()
+logger.setLevel("INFO")
+
+def get_pq_from_bucket(bucket_name, key):
+
+    s3_client = boto3.client('s3')
+
+    try:
+        
+        response = s3_client.get_object(Bucket=bucket_name, Key=key)
+        parquet_data = response['Body'].read()
+        
+        
+        df = pd.read_parquet(io.BytesIO(parquet_data))
+        return df
+    
+    except botocore.exceptions.ClientError as e:
+        print(f"Error accessing S3: {str(e)}")
+        return None
+
---
+++ test/test_get_pq_from_bucket.py
@@ -0,0 +1,56 @@
+from src.loadlambda.get_pq_from_bucket import get_pq_from_bucket
+import pytest
+import pandas as pd
+import boto3
+from botocore.exceptions import ClientError
+from moto import mock_aws
+import io
+
+
+
+@pytest.fixture
+def mock_s3_resource():
+    with mock_aws():
+        yield boto3.resource('s3')
+
+def test_get_pq_from_bucket_success(mock_s3_resource, mocker):
+    
+    bucket_name = 'fake-bucket'
+    key = 'fake-file.parquet'
+    
+    s3 = mock_s3_resource
+    s3.create_bucket(Bucket=bucket_name,CreateBucketConfiguration={'LocationConstraint': 'us-east-2'})
+    
+    fake_data = {'column_1': [1, 2, 3], 'column_2': ['a', 'b', 'c']}
+    mock_df = pd.DataFrame(fake_data)
+    parquet_buffer = io.BytesIO()
+    mock_df.to_parquet(parquet_buffer)
+    
+    s3_object = s3.Object(bucket_name, key)
+    s3_object.put(Body=parquet_buffer.getvalue())
+    
+   
+    mocker.patch('pandas.read_parquet', return_value=mock_df)
+    
+  
+    result = get_pq_from_bucket(bucket_name, key)
+    
+  
+    assert isinstance(result, pd.DataFrame)
+    assert result is (mock_df)    
+
+
+def test_get_pq_from_bucket_error(mocker):
+    
+    mock_s3_client = mocker.Mock()
+    mock_s3_client.get_object.side_effect = ClientError(
+        {'Error': {'Code': 'NoSuchBucket', 'Message': 'The specified bucket does not exist'}},
+        'GetObject'
+    )
+    mocker.patch('boto3.client', return_value=mock_s3_client)
+    
+    # Call the function
+    result = get_pq_from_bucket('non-existent-bucket', 'non-existent-key')
+    
+    # Assert the result
+    assert result is None
