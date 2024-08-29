95a8117a74edcbfb52e19a660a1a90dfa06daa3b
bokney <toknyk@gmail.com>
2024-08-29 10:13:20 +0100

    added mock credentials

--- test/test_db_secrets.py
+++ test/test_db_secrets.py
@@ -4,10 +4,15 @@ from moto import mock_aws
 import boto3
 import pytest
 import json
+import os
 
 @pytest.fixture(scope="function")
 def aws_credentials():
-    pass
+    os.environ["AWS_ACCESS_KEY_ID"] = "test"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "test"
+    os.environ["AWS_SECURITY_TOKEN"] = "test"
+    os.environ["AWS_SESSION_TOKEN"] = "test"
+    os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
 
 @pytest.fixture
 def aws_client(aws_credentials):
